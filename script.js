defe24202fbbe9ba4c86f658a2e14be774ae3fcb
SA1FUU <SA1FUU@users.noreply.github.com>
2024-01-27 18:04:44 +0530

    First Commit

--- script.js
+++ script.js
@@ -1,90 +1,49 @@
 const apiKey = "9252c4e3a127b1c704f9f2422a5a0063"
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
-const rainyIcon = document.querySelector(".rainy")
-const sunnyIcon = document.querySelector(".sunny")
-const cloudyIcon = document.querySelector(".cloudy")
-const mistyIcon = document.querySelector(".misty")
-const thunderIcon = document.querySelector(".thunder")
-
 
 const searchInput = document.querySelector("input")
-const searchButton = document.querySelector(".search img")
+const form = document.querySelector("form")
 
+const loader = document.querySelector(".load-cont")
 
+const error = document.getElementById('error')
 
-searchInput.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-        document.querySelector(".search img").click();
-    }
-});
+const showLoader = () => {
+    loader.style.display = "block"
+}
 
+const hideLoader = () => {
+    loader.style.display = "none"
+}
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
     var data = await response.json()
-    // console.log(data.main.feels_like);
-
-    document.querySelector(".city").innerHTML = data.name
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C"
-    document.querySelector(".feels-like").innerHTML ="Feels Like " + Math.round(data.main.feels_like) + "°C"
-    document.querySelector(".humidity").innerHTML = data.main.humidity + " %"
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h"
-    document.querySelector(".mausam").innerHTML = data.weather[0].main
-
-    if (data.weather[0].main === "Haze") {
-        sunnyIcon.style.display = "block"
-        rainyIcon.style.display = "none"
-        cloudyIcon.style.display = "none"
-        mistyIcon.style.display = "none"
-        thunderIcon.style.display = "none"
-    }
-    else if (data.weather[0].main === "Clouds") {
-        cloudyIcon.style.display = "block"
-        rainyIcon.style.display = "none"
-        sunnyIcon.style.display = "none"
-        mistyIcon.style.display = "none"
-        thunderIcon.style.display = "none"
-    }
-    else if (data.weather[0].main === "Mist") {
-        mistyIcon.style.display = 'block'
-        cloudyIcon.style.display = "none"
-        sunnyIcon.style.display = "none"
-        rainyIcon.style.display = "none"
-        thunderIcon.style.display = "none"
-    }
-    else if (data.weather[0].main === "Rain") {
-        rainyIcon.style.display = "block"
-        mistyIcon.style.display = 'none'
-        cloudyIcon.style.display = "none"
-        sunnyIcon.style.display = "none"
-        thunderIcon.style.display = "none"
-    }
-    else if (data.weather[0].main === "Thunderstorm") {
-        thunderIcon.style.display = "block"
-        rainyIcon.style.display = "none"
-        mistyIcon.style.display = 'none'
-        cloudyIcon.style.display = "none"
-        sunnyIcon.style.display = "none"
-    }
-    else {
-        rainyIcon.style.display = "none"
-        mistyIcon.style.display = 'none'
-        cloudyIcon.style.display = "block"
-        sunnyIcon.style.display = "none"
-        thunderIcon.style.display = "none"
-    }
+    showLoader()
+    // HTML Contents
+    document.querySelector('.temp').textContent = `${Math.floor(data.main.temp)}°C`
+    document.querySelector('.city').textContent = data.name
+    document.querySelector('.feels-like').textContent = `Feels Like ${Math.floor(data.main.feels_like)}°C`
+    document.querySelector('.mausam').textContent = (data.weather[0].main)
+    document.querySelector('.humidity').textContent = `${data.main.humidity} %`
+    document.querySelector('.wind').textContent = `${data.wind.speed} Km/h`
     document.querySelector(".weather").style.display = "block"
-
+    searchInput.value = ""
+    hideLoader()
 }
 
-searchButton.addEventListener("click", () => {
-    checkWeather(searchInput.value)
-    searchInput.value = ""
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    if (searchInput.value == "") {
+        error.textContent = "*Enter City Name"
+    }
+    else {
+        checkWeather(searchInput.value)
+        error.textContent = ""
+    }
 })
 
-const reload = document.querySelector(".reload")
-
-reload.addEventListener("click", () => {
-    location.reload()
+window.addEventListener("load", () => {
+    checkWeather('delhi')
 })
